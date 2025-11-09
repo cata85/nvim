b58048b218740efdf726cf2566d5d5b369b89982
Carter Nelson <carternelson85@yahoo.com>
2025-11-09 16:51:59 -0600

    Swap key mappings for find_files and live_grep

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -16,8 +16,8 @@ return {
                 },
             })
             local builtin = require("telescope.builtin")
-            vim.keymap.set("n", "<leader>fd", builtin.find_files, {})
-            vim.keymap.set("n", "<leader>ff", builtin.live_grep, {})
+            vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+            vim.keymap.set("n", "<leader>fd", builtin.live_grep, {})
             vim.keymap.set("n", ";", "<cmd>lua require('telescope.builtin').resume(require('telescope.themes').get_ivy({}))<CR>", {})
 
             require("telescope").load_extension("ui-select")
